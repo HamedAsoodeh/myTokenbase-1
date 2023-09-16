034d5da4b5806f2574e4414c34dc906e794f6165
Anens0 <129123586+Anens0@users.noreply.github.com>
2023-09-16 07:04:55 -1200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,6 @@ export const TOKEN_CONTRACT_ABI = [
 	{"anonymous": false,"inputs": [{"indexed": true ,
 				"internalType": "address",
 				"name": "spender",
-				"type": "addr"
 			},
 			{
 				"indexed": true,
