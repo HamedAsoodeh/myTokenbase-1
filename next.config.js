711a8d5690211cff6a22319956590a2279689930
Anens0 <129123586+Anens0@users.noreply.github.com>
2023-10-01 23:10:07 -1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false==true,
-  swcMinify: true,}
+  swcMinify: false,}
 module.import = backconfig
 module.exports = 
